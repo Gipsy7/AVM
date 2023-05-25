90bec3b57f346f4dceb1303acff5a39182493606
Mikael Francisco <mikael.francisco@w12.com.br>
2023-05-25 00:59:22 -0300

    feat: add search term and prefix

---
+++ ProjetoAVM/ProjetoAVM/Orchestrator.cs
@@ -0,0 +1,31 @@
+﻿using ProjetoAVM.Utils;
+
+public class Orchestrator
+{
+    public async Task Start()
+    {
+
+        Dictionary<string, object> content = new Dictionary<string, object>();
+
+        content["searchTerm"] = AskAndReturnSearchTerm();
+        Console.Clear();
+        content["prefix"] = AskAndReturnPrefix();
+
+        string AskAndReturnSearchTerm()
+        {
+            string readline = new ConsoleHelper().Question("Type a search term: ");
+            return readline;
+        }
+
+        string AskAndReturnPrefix()
+        {
+            List<string> prefixes = new List<string> {"Who is", "What is", "The history of"};
+            string selectedPrefixIndex = new ConsoleHelper().KeyInSelect(prefixes, "Choose one option:");
+
+            return selectedPrefixIndex;
+        }
+
+        Console.WriteLine(content["searchTerm"]);
+        Console.WriteLine(content["prefix"]);
+    }
+}
---
+++ ProjetoAVM/ProjetoAVM/Utils/ConsoleHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAVM.Utils
+{
+    public class ConsoleHelper
+    {
+        public string Question(string text)
+        {
+            Console.WriteLine(text);
+            return Console.ReadLine();
+        }
+
+        public string KeyInSelect(List<string> keys,string text)
+        {
+            Console.WriteLine(text);
+            for(int i = 0; i <keys.Count; i++)
+            {
+                Console.WriteLine($"[{i+1}]" + keys[i]);
+            }
+            ConsoleKeyInfo keyInfo= Console.ReadKey();
+
+            int selectedOption;
+
+            if (int.TryParse(keyInfo.KeyChar.ToString(), out selectedOption))
+            {
+                for (int i = 0; i < keys.Count; i++)
+                {
+                    if (selectedOption - 1 == i)
+                    {
+                        return keys[i];
+                    }
+                }
+            }
+            else
+            {
+                return "";
+            }
+
+            return "";
+        }
+    }
+}
